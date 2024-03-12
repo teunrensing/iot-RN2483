5a41c12d1dba9d9685108e2cf83363f40ce71217
MarelleV <114147170+MarelleV@users.noreply.github.com>
2024-03-12 14:31:38 +0100

    Minor change

--- han-iot-kiss-lora-master/han-iot-kiss-lora-master/LoRa_TX_RX_Cayenne_HAN/TheThingsNetwork_IOT.cpp
+++ han-iot-kiss-lora-master/han-iot-kiss-lora-master/LoRa_TX_RX_Cayenne_HAN/TheThingsNetwork_IOT.cpp
@@ -1,7 +1,7 @@
 // Copyright © 2017 The Things Network
 // Use of this source code is governed by the MIT license that can be found in the LICENSE file.
 
-#include "TheThingsNetwork.h"
+#include "TheThingsNetwork_IOT.h"
 
 #define debugPrintLn(...)                \
   {                                      \
