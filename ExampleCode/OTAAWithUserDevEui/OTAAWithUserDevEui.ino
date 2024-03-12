c773b1945766b391835930b6ab18e0c612ea09c9
RaymonAlbricht <r.albricht@student.han.nl>
2024-03-12 14:08:16 +0100

    Added first version of OTAAwith deveui

--- ExampleCode/OTAAWithUserDevEui/OTAAWithUserDevEui.ino
+++ ExampleCode/OTAAWithUserDevEui/OTAAWithUserDevEui.ino
@@ -1,9 +1,27 @@
+#include <TheThingsNetwork.h>
+
+const char* AppEUI = "0000000000000000";
+const char* DevEUI = "0000000000000000"; //DEFINE YOUR DEVEUI
+const char* AppKey = "0000000000000000";
+
+
+#define freqPlan TTN_FP_EU868
+
+TheThingsNetwork ttn(loraSerial, debugSerial, freqPlan);
+
 void setup() {
-  // put your setup code here, to run once:
+   loraSerial.begin(57600);
+  debugSerial.begin(9600);
 
-}
+  // Initialize LED output pin
+  pinMode(LED_BUILTIN, OUTPUT);
 
-void loop() {
-  // put your main code here, to run repeatedly:
+  // Wait a maximum of 10s for Serial Monitor
+  while (!debugSerial && millis() < 10000);
 
+  debugSerial.println("-- STATUS");
+  ttn.showStatus();
+
+  debugSerial.println("-- JOIN");
+  ttn.join(appEui, appKey);
 }
